d6cf2f064d7d421320fb6f3d2b57e64c4b83af4b
skillor <skillor@gmx.net>
2023-05-02 15:59:47 +0200

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -3,11 +3,11 @@
 const config = {
   github: {
     username: 'skillor', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
+    sortBy: 'updated', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
-      forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      forks: true, // Forked projects will not be displayed if set to true.
+      projects: ['me', 'skillor'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
